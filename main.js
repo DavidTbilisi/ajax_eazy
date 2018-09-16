4f831863ca075a8b2b10149339b44b8e62531f28
David <davidchincharashvili@gmail.com>
2018-09-16 20:19:33 +0400

    added promises, Post method is working correctly now.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 ;var Ajax = (function (){
     function Ajax(obj) {
-        console.log('კარგად');
         obj = obj==undefined? {} : obj;
         this.req        = new XMLHttpRequest();
         this.method     = obj.method || "GET";
@@ -9,47 +8,53 @@
         this.isSync     = obj.isSync === undefined? '': obj.isSync;
         this.data       = obj.data === undefined? "": obj.data;
         this.headers    = obj.headers === undefined? '': obj.headers;
-        this.promise;
         This = this;
+        this.ok = new Promise(function(resolve, reject){
+
 
         // set handler;
-        this.req.onreadystatechange = function () {
-            console.log(this.readyState);
+        This.req.onreadystatechange = function () {
+            // console.log(this.readyState);
             if (this.readyState == 4 && this.status == 200) {
-                    // done ოპერაცია დასრულებულია
-                    // console.log(this.responseText);
-                        var response = this.responseText;
-                        This.promise = new Promise(function(resolve, reject){
-                            resolve(response)
-                        });
-                        This.ok();
+                // done ოპერაცია დასრულებულია
+                var response = JSON.parse(this.responseText);
+                resolve(response);
+                reject(This.req.status)
             }
         };
         // Open connection and send request;
-        this.req.open(this.method, this.url);
+        This.req.open(This.method, This.url, true);
         // send request
-        if (this.method == "POST") {
-            this.req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-            this.req.send(this.data);
+        if (This.method == "POST" || This.method == 'post') {
+            This.req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            var sendData = '';
+            Object.keys(This.data).forEach(function (p1, p2, p3) {
+                sendData += p1 +"="+ This.data[p1] + "&";
+            });
+            This.req.send(sendData);
+            return this.ok;
         } else {
-            if (this.headers != null) {
-                for (var i = 0; i< this.headers.length; i++) {
-                    var header = this.headers[i].split(',');
-                    this.req.setRequestHeader(header[0],header[1]);
+            if (This.headers != null) {
+                for (var i = 0; i< This.headers.length; i++) {
+                    var header = This.headers[i].split(',');
+                    This.req.setRequestHeader(header[0],header[1]);
                 }
             }
-            this.req.send();
+            This.req.send();
+            return this.ok;
         }
-        Ajax.prototype.ok = function () {
-            return this.promise
-        }
+            reject('false');
+            });
     }
     return Ajax;
 })();
-var ajax = new Ajax();
+var ajax = new Ajax({
+    method:"POST",
+    url:"https://httpbin.org/post",
+    data:{name:"david",lname:'chincharashvili'}
+});
 
-ajax.ok()
-    .then(function(myresponse){
-        console.log(myresponse)
-    });
 
+ajax.ok
+    .then(function(hand) {console.log(hand)})
+    .catch(function(some) {console.log(some)});
